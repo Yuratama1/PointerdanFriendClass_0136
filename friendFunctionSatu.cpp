7f4e626d53bb8b9dbcda8eb9ef8d1d699654fad5
Yuratama1 <yuratama661@gmail.com>
2025-05-06 19:02:18 +0700

    menyelesaikan friend function

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -9,4 +9,13 @@ class mahasiswa{
     friend void setNama(mahasiswa &a, string);
 };
 
-void setNama
+void setNama(mahasiswa &a, string b){
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main(){
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
