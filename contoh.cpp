c071727d379b404b2312dec33d8dbe7a74f96fac
Yuratama1 <yuratama661@gmail.com>
2025-05-06 18:44:15 +0700

    menyelesaikan prosedur dan fungsi

--- contoh.cpp
+++ contoh.cpp
@@ -31,3 +31,15 @@ class Lingkaran{
             return 3.14 * r * r;
         }
 };
+
+int main(){
+    PersegiPanjang objekPP; //membuat objek
+    Lingkaran ol;
+
+    cout << "Masukkan panjang : ";
+    cin >> objekPP.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> objekPP.lebar;
+    cout << "Luas Persegi Panjang : ";
+    objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl;
+}
