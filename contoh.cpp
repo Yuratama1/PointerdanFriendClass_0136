7850ce7997b7108c4af4be5b3486d2e52fdbada2
Yuratama1 <yuratama661@gmail.com>
2025-05-06 18:33:05 +0700

    membuat prosedur persegi panjnag dan lingkaran

--- contoh.cpp
+++ contoh.cpp
@@ -6,6 +6,28 @@ class PersegiPanjang{
         int panjang, lebar;
     public :
         void input(){
-            
+            cout << "Masukkan Panjang : ";
+            cin >> panjang;
+            cout << "Masukkan Lebar : ";
+            cin >> lebar;
         }
-}
+
+        int luasPp(int a, int b){
+            return a * b;
+        }
+        //Lingkaran ol;
+};
+
+class Lingkaran{
+    public:
+        int jarijari;
+
+        void input(){
+            cout << "Masukkan jari-jari : ";
+            cin >> jarijari;
+        }
+
+        int luasLingkaran(int r){
+            return 3.14 * r * r;
+        }
+};
