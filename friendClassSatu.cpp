b8bda3a5e21341c7c515096735c7e7966c1818c4
Yuratama1 <yuratama661@gmail.com>
2025-05-06 19:27:54 +0700

    menyelesaikan code friendclass satu

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class siswa;
-class orang{
+class orang {
     private:
         string nama;
     public:
@@ -12,5 +12,28 @@ class orang{
 };
 
 class siswa{
-
+    private:
+        int id;
+    public:
+        void setId(int pId);
+        void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a){
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama){
+    nama = pNama;
+}
+void siswa::setId(int pId){
+    id = pId;
+}
+
+int main(){
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
